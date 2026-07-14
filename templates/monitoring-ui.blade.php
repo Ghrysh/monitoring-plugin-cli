67d700ef4be847a72fe9fa815fcbba12347a0e0e
Ghrysh <gispi.desu@gmail.com>
2026-07-14 23:12:18 +0700

    Fix env collision and tracker.js deployment

--- templates/monitoring-ui.blade.php
+++ templates/monitoring-ui.blade.php
@@ -1,3 +1,3 @@
 <!-- FutureCloud Visitor Monitoring (Laravel Wrapper) -->
 <!-- Pastikan script ini dimuat sebelum </body> -->
-<script src="{{ asset('vendor/futurecloud/tracker.js') }}"></script>
+<script src="{{ asset('futurecloud/tracker.js') }}"></script>
